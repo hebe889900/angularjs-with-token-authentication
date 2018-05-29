69843f3ed86c58369f50e9ea9534399445254344
Yan <jackyan63@gmail.com>
2018-05-29 11:51:06 -0400

    Setting up MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var User = require('./models/User.js');
 
 var posts = [
     {message: 'hello'},
@@ -8,13 +12,26 @@ var posts = [
 ]
 
 app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/posts', (req, res) => {
     res.send(posts);
 })
 
 app.post('/register', (req, res) => {
-    res.send(req.body);
+    var userData = req.body;
+    var user = new User(userData);
+    user.save((err, result) => {
+        if(err) {
+            console.log('saving user error');
+        }
+        console.log("result");
+        res.sendStatus(200);
+    })  
 })
 
-
+mongoose.connect('mongodb://127.0.0.1:27017/myApp', {
+  })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
 app.listen(3000);
